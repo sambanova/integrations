1ee2c76c037611e940d1f95f808668f7eb0fecd7
snova-jorge <jorge.piedrahita@sambanovasystems.com>
2025-08-22 16:20:59 -0500

    fmt

--- livekit/agent/main.py
+++ livekit/agent/main.py
@@ -20,7 +20,8 @@ class Assistant(Agent):
             instructions='You are the voice assistant named Samba, on behalf of a company called SambaNova. '
             'You can retrieve data via the MCP server.  Be nice. Your interaction with the user will via voice '
             'Expect typos in the input due to transcription but take your best guess to respond to them. '
-            'SambaNova is a hardware company that has developed a new chip called the RDU (reconfigurable data flow unit) that enables fast inference for AI.'
+            'SambaNova is a hardware company that has developed a new chip called the RDU '
+            '(reconfigurable data flow unit) that enables fast inference for AI.'
             "They support a bunch of models including OpenAI's whisper, Meta's llama models, Qwen and DeepSeek"
         )
 
@@ -36,7 +37,8 @@ async def entrypoint(ctx: JobContext) -> None:
     if TTS_PROVIDER.lower() == 'hume':
         tts = hume.TTS(
             voice=PostedUtteranceVoiceWithName(name='Colton Rivers', provider='HUME_AI'),
-            description='The voice exudes calm, serene, and peaceful qualities, like a gentle stream flowing through a quiet forest.',
+            description='The voice exudes calm, serene, and peaceful qualities, '
+            'like a gentle stream flowing through a quiet forest.',
         )
     elif TTS_PROVIDER.lower() == 'elevenlabs':
         tts = elevenlabs.TTS(voice_id='ODq5zmih8GrVes37Dizd', model='eleven_multilingual_v2')  # type: ignore[assignment]
